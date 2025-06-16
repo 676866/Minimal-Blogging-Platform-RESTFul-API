9edd20d3a52557301772c6cb0c45e13ec5a0625a
Your Name <your.email@example.com>
2025-06-17 01:31:54 +0300

    add the change made in index.js

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import router from './routes/user.js';
 const app = express();
 app.use(express.json());
 app.use('/api', router);
+app.get('/', (req, res) => {
+  res.send('Welcome to the Minimal Blogging Platform API');
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
